aa79e12185d185e5335fc2f27015b63d9d206916
asanmor916 <asanmor916@iesalmudeyne>
2023-10-24 14:24:21 +0200

    Segundo commit: Creación de constructor con parámetros (Álvaro Sánchez Moreno)

--- instituto.java
+++ instituto.java
@@ -3,4 +3,11 @@ public class Instituto {
     String ciudad;
     String codigo;
     int clases;
+
+    public Instituto(String nombre, String ciudad, String codigo, int clases) {
+        this.nombre=nombre;
+        this.ciudad=ciudad;
+        this.codigo=codigo;
+        this.clases=clases;
+    }
 }

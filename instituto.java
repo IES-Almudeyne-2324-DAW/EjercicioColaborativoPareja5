901707c2f882465f2fbc8b791664476c5bead7ea
pmargom596 <pmargom596@iesalmudeyne.es>
2023-10-24 14:29:08 +0200

    Tercer commit: Setters y método cambiarNombre

--- instituto.java
+++ instituto.java
@@ -10,4 +10,24 @@ public class Instituto {
         this.codigo=codigo;
         this.clases=clases;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setclases(int clases) {
+        this.clases = clases;
+    }
+
+    public String cambiarNombre(String nombre) {
+        return nombre = "IES Al-mudeyne";
+    }
 }
